dbe2803c147d49d7c213f88f991b5b7d79358c3d
Vincent Letourmy <letourmyvincent@gmail.com>
2020-01-23 08:03:04 +0100

    yo

---
+++ netshowCompil.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gcc -o "netshow" /home/vegale/"Vincent Letourmy"/Documents/NETSHOW/Atelier/netshow.c
+gcc -o "netshow_ip" /home/vegale/"Vincent Letourmy"/Documents/NETSHOW/Atelier/netshow_ip.c
+gcc -o "netshow_lname" /home/vegale/"Vincent Letourmy"/Documents/NETSHOW/Atelier/netshow_lname.c
